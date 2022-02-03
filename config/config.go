9d8f9b312077db7fbb3dff8ba31d54b31723f6b5
eldy.hidayat <eldy.hi@gmail.com>
2022-02-03 19:25:37 +0700

    migrate repo, add schema graphql, add docker file, docker compose, add env on config

---
+++ config/config.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"os"
+	"sync"
+
+	"github.com/labstack/gommon/log"
+	"github.com/spf13/viper"
+)
+
+//AppConfig Application configuration
+type AppConfig struct {
+	Port     int `yaml:"port"`
+	Database struct {
+		Driver   string `yaml:"driver"`
+		Name     string `yaml:"name"`
+		Address  string `yaml:"address"`
+		Port     int    `yaml:"port"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+	}
+}
+
+var lock = &sync.Mutex{}
+var appConfig *AppConfig
+
+//GetConfig Initiatilize config in singleton way
+func GetConfig() *AppConfig {
+	lock.Lock()
+	defer lock.Unlock()
+
+	if appConfig == nil {
+		appConfig = initConfig()
+	}
+
+	return appConfig
+}
+
+func initConfig() *AppConfig {
+	var defaultConfig AppConfig
+	defaultConfig.Port = 8080
+	defaultConfig.Database.Driver = "mysql"
+	defaultConfig.Database.Name = os.Getenv("DB_NAME")
+	defaultConfig.Database.Address = os.Getenv("DB_ADDRESS")
+	defaultConfig.Database.Port = 3306
+	defaultConfig.Database.Username = os.Getenv("DB_USERNAME")
+	defaultConfig.Database.Password = os.Getenv("DB_PASSWORD")
+
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("./config/")
+
+	if err := viper.ReadInConfig(); err != nil {
+		// log.Info("error to load config file, will use default value ", err)
+		return &defaultConfig
+	}
+
+	var finalConfig AppConfig
+	err := viper.Unmarshal(&finalConfig)
+	if err != nil {
+		log.Info("failed to extract config, will use default value")
+		return &defaultConfig
+	}
+
+	return &finalConfig
+}
